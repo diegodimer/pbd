ce515d121eb617c9b394d745d6d4720a58f824a8
Diego Dimer <diego.dimer@ufrgs.br>
2021-11-26 20:04:54 -0300

    add missing output

--- funcionalidades_basicas/most_talkative_users.sql
+++ funcionalidades_basicas/most_talkative_users.sql
@@ -1,6 +1,9 @@
 -- top 10 writers about apple
 select t.writer, count(t.tweet_id) as username from
 (select tweet_id from company_tweet ct where symbol = 'AAPL') ct join tweet t on ct.tweet_id = t.tweet_id 
-group by t.writer order by count(t.tweet_id) desc limit 10;
-
-
+group by t.writer order by count(t.tweet_id) desc limit 5;
+-- _peripherals	    91424
+-- computer_hware	90899
+-- MacHashNews	    42190
+-- PortfolioBuzz	38719
+-- treabase	        13828
